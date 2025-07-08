e87fca410c14e78e3ae3e0eb89fee0ce76a44731
Azevedothuu <gamesazevedo01@gmail.com>
2025-07-08 20:47:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  //img agora
+  const img = document.querySelector("#profile img");
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+  }
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "homem de oculos preto e casaco preto");
+  } else {
+    img.setAttribute("alt", "homem de camisa preta sorrindo");
+  }
+}
